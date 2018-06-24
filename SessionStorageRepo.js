28b40e6d4e2aeacd15e88c31190c46ad665c4dae
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-06-24 19:27:16 +0200

    Object pattern

--- SessionStorageRepo.js
+++ SessionStorageRepo.js
@@ -2,6 +2,7 @@
  * Repo generator that uses only one unique key of the sessionStorage
  * in a unique Objects Array . [{},{},...]
  *
+ * Object pattern.
  * @param repoNameStr Repo's name , is a unique key of the sessionStorage.
  * @param metadataIdStr Is the unique key that we can use in the find operation
  * @returns {{create: create, read: (function(*, *): *), update: update, delete: del}}
@@ -25,45 +26,33 @@ function SessionStorageRepo(repoNameStr, metadataIdStr) {
   this.repo = JSON.parse(this.rawRepo);
 
   //publics
-  function create(item) {
+  this.create = function create(item) {
     if (read(metadataIdStr, item.metadataIdStr)) {
         throw "element exists, delete first or update";
     }
     this.repo.push(item);
     sessionStorage.setItem(this.repoNameStr, JSON.stringify(this.repo));
-  }
+  };
 
-  function read(key, value) {
+  this.read = function read(key, value) {
     return this.repo.filter(function (item) {
       return (item.key === value);
     })
-  }
+  };
 
-  function update() {
+  this.update = function update() {
+    //todo stub
+  };
+
+  this.del = function del() {
     //todo stub
   }
-
-  function del() {
-    //todo stub
-  }
-
-  function _exists(key, value) {
-    read(key, value);
-  }
-
-  return {
-    create: create,
-    read: read,
-    update: update,
-    delete: del
-  }
-
 }
 
 
-var namespace = namespace || {};
-namespace.SessionStorageRepo = SessionStorageRepo;
+var PCC = PCC || {};
+PCC.SessionStorageRepo = SessionStorageRepo;
 
 var exports = exports || {};
-exports.namespace = namespace;
+exports.PCC = PCC;
 
