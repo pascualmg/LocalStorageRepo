216a57f8f69e72241bca5256d3820dcb5e010933
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-06-25 00:41:44 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,2 +1,17 @@
 repoFoo = (new namespace.SessionStorageRepo("foo" , "idFoo"))
-repoFoo.create({idFoo:"11334", data: "8==D"})
+try {
+  repoFoo.create({idFoo:"11334", data: "8==D"})
+repoFoo.create({idFoo:"11335", data: "8==D"})
+repoFoo.create({idFoo:"11337", data: "8==D"})
+
+  var ticker = 100;
+  while(ticker-- >= 0){
+    repoFoo.create({idFoo:ticker, data: "8==D" + ticker})
+  }
+
+}catch(e) {
+  console.log(e)
+}
+
+  console.log(repoFoo.read("data", "8==D"))
+  console.log(repoFoo.read("idFoo", "11334")[0])
