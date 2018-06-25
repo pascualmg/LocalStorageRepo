83ef926eac688f9b8d03df42093f22babb7ca651
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-06-25 03:12:23 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,14 +1,19 @@
 repoFoo = (new namespace.SessionStorageRepo('foo', 'idFoo'))
 try {
   //fixtures
-  for (i = 100; i >= 0; i--) {
+  for (i = 1000; i >= 0; i--) {
     repoFoo.create({
       idFoo: i,
-      data: "data" + i
+      data: 'data' + i
     })
   }
 } catch (e) {
   console.log(e)
 }
 console.log(repoFoo.read('data', '8==D'))
-console.log(repoFoo.read('idFoo', '11334') [0])
+console.log(repoFoo.read('idFoo', '999') [0])
+
+  for (i = 500; i >= 0; i--) {
+    repoFoo.del(i)
+  }
+
