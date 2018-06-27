5b4df5f72d1d94b2d979a627faafb8c12241f25d
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-06-27 10:14:39 +0200

    del rulando

--- LocalStorageRepo.js
+++ LocalStorageRepo.js
@@ -16,62 +16,57 @@ function LocalStorageRepo(repoNameStr, metadataIdStr) {
 
   this.repoNameStr = repoNameStr || function () {
     throw ("default parameter constructor repoNameStr missing")
-  }
+  };
   this.metadataIdStr = metadataIdStr || function () {
     throw ("default parameter constructor metadataIdStr missing")
-  }
-  var voidRepo = []
-  localStorage.getItem(repoNameStr) || localStorage.setItem(repoNameStr, JSON.stringify(voidRepo))
-  this.repo = JSON.parse(localStorage.getItem(repoNameStr))
+  };
+  var voidRepo = [];
+  localStorage.getItem(repoNameStr) || localStorage.setItem(repoNameStr, JSON.stringify(voidRepo));
+  this.repo = JSON.parse(localStorage.getItem(repoNameStr));
   //publics
   this.create = function create(item) {
-    var id2Create = item[this.metadataIdStr]
+    var id2Create = item[this.metadataIdStr];
     if (this._exists(this.metadataIdStr, id2Create)) {
       throw "element exists, delete first or update"
     }
-    this.repo.push(item)
+    this.repo.push(item);
     this._flushRepo(this.repo)
-  }
+  };
   this.read = function read(key, value) {
     return this.repo.filter(function (item) {
-      //object as array dereference == 4 lazy matching id "2" like id 2
-      return (item[key] == value)
+      return (item[key] == value)//lazy comparation
     })
-  }
+  };
   this.findOneById = function findOneByid(id) {
     return this.read(this.metadataIdStr, id) [0]
-  }
+  };
   this.update = function update(criteria, payload) {
     //todo
-  }
+  };
   this.del = function del(id) {
     if (!this._exists(id)) {
-      console.info("element with id " + id + "does not exist")
+      console.info("element with id " + id + "does not exist");
       return false
     }
 
-    var mutatedRepo = this.repo.filter.call(this, function (item) {
-      //todo:
-      return item["idFoo"] != id
-    })
-
-    this.repo = mutatedRepo
-    this._flushRepo(this.repo)
-  }
+    this._flushRepo(this.repo.filter(function (item) {
+      return item[this.metadataIdStr] != id//lazy compraration
+    }, this))
+  };
 
   //stub not working , delete if not used
   this._indexOf = function _indexOf(id) {
-    var index = undefined
+    var index = undefined;
     this.repo.forEach(function (item, it) {
       if (item[this.metadataIdStr] == id) {
         index = it
       }
-    })
+    });
     return index
-  }
+  };
   this._exists = function _exists(key, value) {
     return this.read(key, value).length
-  }
+  };
   this._flushRepo = function _flushRepo(repo) {
     localStorage.setItem(repoNameStr, JSON.stringify(repo))
   }
